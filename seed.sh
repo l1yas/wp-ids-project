cd73888de733e61ade4f624089e4ede68877b0cb
Ilyas <ilyasyahiacherif@gmail.com>
2026-04-25 17:26:48 +0100

    Add files via upload

---
+++ seed.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "waiting for MySQL..."
+
+until docker compose exec db mysqladmin ping -h "localhost" --silent; do
+  sleep 2
+done
+
+echo "inserting users..."
+
+docker compose exec -T db mysql -u wpuser -pwppass wordpress <<EOF
+
+INSERT INTO wp_users (user_login, user_pass, user_email, user_registered)
+SELECT 'user', MD5('user123'), 'user@test.local', NOW()
+WHERE NOT EXISTS (
+  SELECT 1 FROM wp_users WHERE user_login='user'
+);
+
+INSERT INTO wp_usermeta (user_id, meta_key, meta_value)
+SELECT ID, 'wp_capabilities', 'a:1:{s:10:"subscriber";b:1;}'
+FROM wp_users WHERE user_login='user'
+ON DUPLICATE KEY UPDATE meta_value=meta_value;
+
+EOF
+
+echo "done"
