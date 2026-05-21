485a4dc8fb2689c1bb4e36f7e0fb1435584d8f8d
Ilyas <ilyasyahiacherif@gmail.com>
2026-05-21 21:05:27 +0100

    Update wp_simple_history.py

--- parsers/wp_simple_history.py
+++ parsers/wp_simple_history.py
@@ -1,28 +1,64 @@
 import subprocess
 import re
 
+failed_login_pattern = re.compile(r'username "([^"]+)"')
+
 def fetch_logs():
         result = subprocess.run(
-                ["wp", "simple-history", "list", "--allow-root"],
+                ["docker", "exec", "wordpress-wordpress-1", "wp", "simple-history", "list", "--allow-root"],
                 capture_output=True,
                 text=True
         )
         return result.stdout
 
+def parse_line(line):
+        parts = line.split("\t")
+        if len(parts)<4:
+                return None
+        return {
+                "id": parts[0],
+                "date": parts[1],
+                "initiator": parts[2],
+                "description": parts[3],
+                "level": parts[-2] if len(parts)>2 else None,
+                "raw": line
+        }
+
 def failed_logins(raw):
         events=[]
         for line in raw.split("\n"):
-                if "Failed to login" in line:
-                        user_match = re.search(r'user name "[(^"]+)"', line)
-                        username = user_match.group(1) if user_match else None
-                        events.append({
-                                "type": "failed_login",
-                                "username": username,
-                                "ip": None,
-                                "raw": line.strip()
-                        })
+                if "Failed to login" not in line:
+                        continue
+
+                parsed = parse_line(line)
+                if not parsed:
+                        continue
+
+                match = failed_login_pattern.search(parsed["description"])
+                username = match.group(1) if match else None
+
+                events.append({
+                        "type": "failed_login",
+                        "username": username,
+                        "date" : parsed["date"],
+                        "level": parsed["level"],
+                        "raw": line
+                })
         return events
 
 def get_failed_logins_events():
         raw = fetch_logs()
         return failed_logins(raw)
+
+
+def debug():
+        raw = fetch_logs()
+        print("=== RAW LOGS ===")
+        print(raw)
+        events = failed_logins(raw)
+        print("\n=== EVENTS ===")
+        print(events)
+
+
+if __name__ == "__main__":
+        debug()
